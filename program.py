4c23250813932e19a8627789ee849a6b449d3ac0
RANJANA P <ranjanap24bit045@skasc.ac.in>
2025-07-24 21:49:37 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,155 @@
+# Program 1
+print("Name:Ranjana")
+# Output:
+# Name:Ranjana
+
+# Program 2
+print("Age:18")
+# Output:
+# Age:18
+
+# Program 3
+print("Class:Bsc IT'A'")
+# Output:
+# Class:Bsc IT'A'
+
+# Program 4
+if 5 > 4:
+  print("Five is greater than four!")
+# Output:
+# Five is greater than four!
+
+# Program 5
+x = 45
+y = "Ranjana"
+print(x)
+print(y)
+# Output:
+# 45
+# Ranjana
+
+# Program 6
+# This is a comment.
+print("python is a programming Languages")
+# Output:
+# python is a programming Languages
+
+# Program 7
+#print("sri krishna!")
+print("College")
+# Output:
+# College
+
+# Program 8
+"""This is a comment
+written in
+more than a single quotes"""
+print("Hello, World!")
+# Output:
+# Hello, World!
+
+# Program 9
+# This is a comment
+# this is a multiline comment 
+# more than one line
+print("Hello, World!")
+# Output:
+# Hello, World!
+
+# Program 10
+x1 = 4
+x2 = "Hello"
+print(x1)
+# Output:
+# 4
+
+# Program 11
+x = str(4)
+y = int(3)
+z = float(3.37)
+print(x)
+print(y)
+print(z)
+# Output:
+# 4
+# 3
+# 3.37
+
+# Program 12
+x = 5.5
+y = "M"
+print(type(x))
+print(type(y))
+# Output:
+# <class 'float'>
+# <class 'str'>
+
+# Program 13
+x = y = z = "awesome"
+print(x)
+print(y)
+print(z)
+# Output:
+# awesome
+# awesome
+# awesome
+
+# Program 14
+x, y, z = "apple","papaya","mango"
+print(x)
+print(y)
+print(z)
+# Output:
+# apple
+# papaya
+# mango
+
+# Program 15
+x = 15
+y = 15
+print(x + y)
+# Output:
+# 30
+
+# Program 16
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+print("Python is " + x)
+# Output:
+# Python is fantastic
+
+# Program 17
+a = "Ranjana!"
+print(a.upper())
+# Output:
+# RANJANA!
+
+# Program 18
+a = "jello, World!"
+print(a.replace("j", "h"))
+# Output:
+# hello, World!
+
+# Program 19
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+# Output:
+# HelloWorld
+
+# Program 20
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
